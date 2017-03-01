7e807b9bf219d7dc3a67ef582e0c9a19d35219f4
Bruno Lins <brunovpl@gmail.com>
2017-03-01 13:32:25 -0300

    Making the forecast list clickable

--- app/src/main/java/com/example/android/kotlinforandroiddevs/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/android/kotlinforandroiddevs/ui/activities/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.android.kotlinforandroiddevs.ui.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import com.example.android.kotlinforandroiddevs.ui.adapters.ForecastListAdapter
 import com.example.android.kotlinforandroiddevs.R
 import com.example.android.kotlinforandroiddevs.domain.commands.RequestForecastCommand
+import com.example.android.kotlinforandroiddevs.domain.model.Forecast
+import com.example.android.kotlinforandroiddevs.ui.adapters.ForecastListAdapter
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +24,12 @@ class MainActivity : AppCompatActivity() {
         doAsync {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
-                forecastList.adapter = ForecastListAdapter(result)
+                forecastList.adapter = ForecastListAdapter(result,
+                        object : ForecastListAdapter.OnItemClickListener {
+                            override fun invoke(forecast: Forecast) {
+                                toast(forecast.date)
+                            }
+                        })
             }
         }
     }
