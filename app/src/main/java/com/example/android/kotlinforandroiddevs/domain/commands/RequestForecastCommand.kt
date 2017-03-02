29a0de0206fa4f5441df191dcf57f4b6b30cc90d
Bruno Lins <brunovpl@gmail.com>
2017-03-02 15:11:58 -0300

    Visibility modifiers

--- app/src/main/java/com/example/android/kotlinforandroiddevs/domain/commands/RequestForecastCommand.kt
+++ app/src/main/java/com/example/android/kotlinforandroiddevs/domain/commands/RequestForecastCommand.kt
@@ -4,7 +4,7 @@ import com.example.android.kotlinforandroiddevs.data.ForecastRequest
 import com.example.android.kotlinforandroiddevs.domain.mappers.ForecastMapper
 import com.example.android.kotlinforandroiddevs.domain.model.ForecastList
 
-class RequestForecastCommand(val zipCode: String): Command<ForecastList> {
+class RequestForecastCommand(private val zipCode: String): Command<ForecastList> {
     override fun execute(): ForecastList {
         val forecastRequest = ForecastRequest(zipCode)
         return ForecastMapper().convertFromDataModel(forecastRequest.execute())
